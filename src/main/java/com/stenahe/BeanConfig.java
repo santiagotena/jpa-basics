65324d307fa12ef7936fee529e4e897687d04541
Santiago Tena Hernandez <stenahe@gmail.com>
2023-09-28 01:19:43 +0200

    Add @Bean example

--- src/main/java/com/stenahe/BeanConfig.java
+++ src/main/java/com/stenahe/BeanConfig.java
@@ -1,10 +1,16 @@
 package com.stenahe;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
 
 @Configuration
 @ComponentScan(basePackages = "com.stenahe")
 public class BeanConfig {
+
+    @Bean
+    public Doctor doctor() {
+        return new Doctor();
+    }
+
 }
--- src/main/java/com/stenahe/Doctor.java
+++ src/main/java/com/stenahe/Doctor.java
@@ -2,7 +2,6 @@ package com.stenahe;
 
 import org.springframework.stereotype.Component;
 
-@Component
 public class Doctor implements Staff {
     public void assist() {
         System.out.println("Doctor is assisting");
