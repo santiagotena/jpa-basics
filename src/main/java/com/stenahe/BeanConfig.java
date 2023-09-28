9d831dcc456699a0cbca3efe47b7317d08c0e2d6
Santiago Tena Hernandez <stenahe@gmail.com>
2023-09-28 14:11:36 +0200

    Format

--- src/main/java/com/stenahe/BeanConfig.java
+++ src/main/java/com/stenahe/BeanConfig.java
@@ -12,5 +12,4 @@ public class BeanConfig {
     public Doctor doctor() {
         return new Doctor();
     }
-
 }
--- src/main/java/com/stenahe/Doctor.java
+++ src/main/java/com/stenahe/Doctor.java
@@ -1,7 +1,5 @@
 package com.stenahe;
 
-import org.springframework.stereotype.Component;
-
 public class Doctor implements Staff {
     public void assist() {
         System.out.println("Doctor is assisting");
